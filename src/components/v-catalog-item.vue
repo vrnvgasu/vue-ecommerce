36837823bf1f4e0628f393edbcb0a4d4112ce277
Dmitrii Shitikov <dshitikov@rtech.ru>
2022-01-08 17:24:17 +0300

    добавили 2 компонента: каталог, страница товара, корзина

---
+++ src/components/v-catalog-item.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="v-catalog-item"></div>
+</template>
+
+<script>
+export default {
+  name: "v-catalog-item"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/v-catalog.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="v-catalog"></div>
+</template>
+
+<script>
+export default {
+  name: "v-catalog"
+}
+</script>
+
+<style>
+
+</style>
