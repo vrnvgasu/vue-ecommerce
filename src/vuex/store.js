3857143f7a9b1b2f3ac718d267057d753a1b6e05
Dmitrii Shitikov <dshitikov@rtech.ru>
2022-01-08 23:28:43 +0300

    увеличение кол-ва заказа определенного товара

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -14,7 +14,21 @@ let store = new Vuex.Store( {
       state.products = products;
     },
     SET_CART: (state, product) => {
-      state.cart.push(product);
+      if (state.cart.length) {
+        let isProductExists = false;
+        state.cart.map(function (item) {
+          if (item.article === product.article) {
+            isProductExists = true;
+            item.quantity++;
+          }
+        });
+
+        if (!isProductExists) {
+          state.cart.push(product);
+        }
+      } else {
+        state.cart.push(product);
+      }
     },
     REMOVE_FROM_CART: (state, index) => {
       state.cart.splice(index, 1);
