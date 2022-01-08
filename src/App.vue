ae114ad963053446c074ee929dc755dab7fc3940
Dmitrii Shitikov <dshitikov@rtech.ru>
2022-01-08 17:22:18 +0300

    импортировали первый компонент v-main-wrapper в корневой компонент App

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
+    <h1>Hello world!</h1>
+    <v-main-wrapper /> <!--Первый дочерний компонент-->
   </div>
 </template>
 
 <script>
+// импортируем первый компонент
+import vMainWrapper from './components/v-main-wrapper';
 
 export default {
   name: 'App',
   components: {
+    vMainWrapper, // дочерний компонент
   }
 }
 </script>
